83fa4da190fe4188852bf338e7f027f2d6301378
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-07 12:49:15 -0500

    constructor

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class Drink {
+    private String name;
+    private double price;
+
+    public Drink(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 }
